8af392a9cd11d1f52f5d838faa05d2efa279aacb
jarulnathan <justin@gizmag.com>
2014-08-19 16:38:42 +1000

    bump version number

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_require = [
 
 setup(
     name='django-haystack',
-    version='2.2.0',
+    version='2.2.1',
     description='Pluggable search for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
